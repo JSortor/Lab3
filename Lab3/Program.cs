d5127ae154bf71b3cc1988f8cb3b4c9e4a686b07
Justin <justinsortor@gmail.com>
2019-01-15 15:52:17 -0500

    First Commit

---
+++ Lab3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool runP = true;
+            Console.WriteLine("Welcome, Please enter your first name");
+            string firstName = Console.ReadLine();
+
+            while (runP == true)
+            {
+                Console.WriteLine("Hello {0}, Please enter an interger value between 1 and 100",
+                    firstName);
+                int value = int.Parse(Console.ReadLine());
+                Console.WriteLine("\n{0},You entered {1}, lets perform the conditional actions!", firstName,
+                    value);
+                if (value % 2 == 0 && value >= 2 && value < 25)
+                {
+                    Console.WriteLine("{0}, You entered {1}, this choice is even and less than 25", firstName, value);
+                }
+                else if (value % 2 == 0 && value > 25 && value <= 60)
+                {
+                    Console.WriteLine("{0}, You entered {1}, this choice is even", firstName, value);
+                }
+                else if (value % 2 == 0 && value >= 60 && value <= 100)
+                {
+                    Console.WriteLine("{0}, You entered {1}, this choice is even", firstName,  value);
+                }
+                else if (value % 2 != 0 && value >= 60 && value <= 100)
+                {
+                    Console.WriteLine("{0}, You entered {1}, this choice is odd", firstName, value);
+                }
+                else if (value % 2 != 0 && value > 1 && value <= 100)
+                {
+                    Console.WriteLine("{0}, You entered {1}, this choice is odd", firstName, value);
+                }
+                else if (value % 2 != 0 || value % 2 == 0 && value < 1 || value > 100)
+                {
+                    Console.WriteLine("{0}, {1} Is not an integer between 1 and 100", firstName, value);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid entry");
+                }
+                runP = Continue();
+            }
+        }
+        public static bool Continue()
+        {
+            Console.WriteLine("\nWould you like to run the application again? (Y or N)");
+            string input = Console.ReadLine().ToLower();
+            input = input.ToLower();
+            bool goOn;
+            if (input == "y")
+            {
+                goOn = true;
+            }
+            else if (input == "n")
+            {
+                Console.WriteLine("Thank you, Goodbye!");
+                Console.ReadLine();
+                goOn = false;
+            }
+            else
+            {
+                Console.WriteLine("I don't understand that, let's try again");
+                goOn = Continue();
+            }
+            return goOn;
+        }
+    }
+}
